cf886699d8e846078dbb7eee5d2dd70ebc809786
JosephTheSmartPers <kupakovacsdummy@gmail.com>
2022-10-06 10:01:28 +0200

    qrcode bit better

--- SlashCommands/Utility/qrcode.js
+++ SlashCommands/Utility/qrcode.js
@@ -10,12 +10,12 @@ module.exports = {
 
         let qrcode = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${args.getString("message").split(" ").join("%20")}`
         interaction.reply({content: "Sent qrcode<a:Check:831956237305643069>", ephemeral: true})
-        console.log(qrcode)
+        let msg = interaction.channel.send(`Searching minecraft for \`${ip}\` <a:loading:1026905223031173150>`)
         const embed = new EmbedBuilder()
             .setTitle("Scan this!")
             .setImage(qrcode)
             .setColor("NotQuiteBlack")
-        interaction.channel.send({embeds: [embed]})
+        msg.edit({content: "",embeds: [embed]})
 
 	}
 }
