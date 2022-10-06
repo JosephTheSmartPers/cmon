b19a623e1a1790a999bfc5735fdbb0004008d33e
JosephTheSmartPers <kupakovacsdummy@gmail.com>
2022-10-06 14:32:12 +0200

    botinfo fixed

--- commands/📁Support commands/botinfo.js
+++ commands/📁Support commands/botinfo.js
@@ -20,20 +20,22 @@ module.exports = {
         let chansize = client.channels.cache.size
         let uptimxd = client.uptime 
         let servsize = client.guilds.cache.size
-        let botembed = new Discord.MessageEmbed()
+        let botembed = new Discord.EmbedBuilder()
         .setColor("#00ff00")
         .setThumbnail(bicon)
-        .addField("🤖Bot Name", `${client.user.username}`, inline)
-        .addField("👑Bot Owner", "<@483519738727759873>", inline )
-        .addField("🛡Servers", `${servsize}`, inline)
-        .addField("💬Channels", `📁 ${chansize}`, inline)
-        .addField("👥Users", `${usersize}`, inline)
-        .addField("📚Bot Library", "Discord.js", inline)
-        .addField("⚙️Commands", `${client.commands.size}`, inline)
-        .addField("• Discord.js", `v${version}`, inline)
-        .addField("Invite", `[Click here for invite.](https://discord.com/oauth2/authorize?client_id=836893540427759646&scope=bot&permissions=8589934591)`, inline)
-        .addField("📆Created On", client.user.createdAt.toString())
-                .setFooter(`ℹ️Information about: ${client.user.username}. Developed by: Barni#0811`)
+        .addFields(
+                {name: "🤖Bot Name", value: `${client.user.username}`, inline: true},
+                {name: "👑Bot Owner", value: "<@483519738727759873>", inline: true},
+                {name: "🛡Servers", value: `${servsize}`, inline: true},
+                {name: "💬Channels", value: `📁 ${chansize}`, inline: true},
+                {name: "👥Users", value: `${usersize}`, inline: true},
+                {name: "📚Bot Library", value: "Discord.js", inline: true},
+                {name: "⚙️Commands", value: `${client.commands.size}`, inline: true},
+                {name: "• Discord.js", value: `v${version}`, inline: true},
+                {name: "Invite", value: `[Click here for invite.](https://discord.com/oauth2/authorize?client_id=836893540427759646&scope=bot&permissions=8589934591)`, inline: true},
+                {name: "📆Created On", value: client.user.createdAt.toString()}
+        )
+                .setFooter({text: `ℹ️Information about: ${client.user.username}. Developed by: Barni#0811`})
        
         message.channel.send({embeds: [botembed]});
      }
