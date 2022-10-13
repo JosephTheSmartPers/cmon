d8925aaba8e72d2069034eb3fe1cd62bdc3f073e
JosephTheSmartPers <kupakovacsdummy@gmail.com>
2022-10-13 14:43:49 +0200

    trivia work plis

--- SlashCommands/Economy/trivia.js
+++ SlashCommands/Economy/trivia.js
@@ -15,58 +15,43 @@ module.exports = {
         
         let sal = 20000
 
-        await interaction.reply(`Searching the for a question to ask <a:loading:1026905223031173150>`)
-
-        async function work(question, answer){
-        const modal = new ModalBuilder()
-			.setCustomId(user.id)
-			.setTitle('Answer correctly!');
-        
-        const Wquestion = new TextInputBuilder()
-			.setCustomId('triviaQuestion')
-		    // The label is the prompt the user sees for this input
-			.setLabel(question)
-		    // Short means only a single line of text
-			.setStyle(TextInputStyle.Paragraph);
-        const firstActionRow = new ActionRowBuilder().addComponents(Wquestion);
-
-        modal.addComponents(firstActionRow);
-
-        interaction.showModal(modal)
-
-        const filter = i =>{
-            return i.user == user
-        }
-
-        interaction.awaitModalSubmit({filter, time: 15_000 }).then(async inter=> {
-
-            let hisAnsw = inter.fields.getTextInputValue("triviaQuestion")
-            if(hisAnsw.toLowerCase() == answer){
-                await profileModel.findOneAndUpdate({userID: user.id,}, 
-                    {$inc: {moniy: sal,},
-                }); 
-            const fitEmbed = new EmbedBuilder()
-                .setColor('#fff85f')
-                .setAuthor({name: user.tag, iconURL: user.displayAvatarURL({ dynamic: true })})
-                .setTitle(`Youg got ${sal} moniy for answering correctly!`)
-            inter.reply({embeds: [fitEmbed]});
-            }else{
-                inter.reply("That wasn't the answer, better luck next time!")
-                }
-        })}
+        async function working(question, answer){
+            const filter = m => m.author.id === interaction.user.id;
+            const rusure = await interaction.reply(question)
+            const collector = await interaction.channel.createMessageCollector({
+                max: 1,
+                filter,
+                time: 1000 * 20
+            })
+    
+            collector.on('collect', async m =>{
+                if(m.content.toLowerCase() == answer) {
+                        await profileModel.findOneAndUpdate({userID: interaction.user.id,}, 
+                            {$inc: {moniy: sal,},
+                        }); 
+                    const fitEmbed = new Discord.EmbedBuilder()
+                        .setColor('#fff85f')
+                        .setAuthor({name: interaction.user.tag, iconURL: interaction.user.displayAvatarURL({ dynamic: true })})
+                        .setTitle(`After a nice day of work u got ${sal} moniy!`)
+                    m.channel.send({embeds: [fitEmbed]});
+                }else{
+                    m.reply("That wasn't the answer, better luck next time!")
+                    }})
+            collector.on('end', message =>{return})
+    }
         
         let question = ""
         
         await fetch("https://jservice.io/api/random?count=1")
         .then(response => {
             if(!response.ok){
-                return interaction.editReply("something went wrong :(")
+                return interaction.reply("something went wrong :(")
             }
             return response.json()
         })
         .then(async data => {
             question = await data
-        }).catch(err=>{return interaction.editReply("Something messed up ):")})
+        }).catch(err=>{return interaction.reply("Something messed up ):")})
         
         work(question[0].question, question[0].answer)
 
