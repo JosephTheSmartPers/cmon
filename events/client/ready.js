7803f2171320c62b41bc008c11e04f7a117e68c6
JosephTheSmartPers <kupakovacsdummy@gmail.com>
2023-11-19 09:44:09 +0100

    notify on phone

--- events/client/ready.js
+++ events/client/ready.js
@@ -13,7 +13,11 @@ module.exports = async (Discord, client, message) => {
   let channel = await client.channels.cache.find(
     (channel) => channel.id == "957553977896095764"
   );
-  channel.send("BarniBot online!");
+  fetch("https://ntfy.sh/BarniBot", {
+    method: "POST",
+    headers: { Title: "BarniBot online", Tags: "white_check_mark" },
+    body: "Lets go",
+  });
   console.log(`BarniBot is online`);
   client.user.setActivity(`-help | ${client.guilds.cache.size} servers`, {
     type: "LISTENING",
