e6fcb427583c9718a815498a06017debf086ec0a
JosephTheSmartPers <kupakovacsdummy@gmail.com>
2023-11-19 09:10:08 +0100

    stupid changes

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,25 +1,23 @@
 const message = require("../guild/messageCreate");
-const translate = require("translate"); 
-const Discord = require('discord.js');
-const offline = require('../../mpassword/offline')
-const util = require('minecraft-server-util');
+const translate = require("translate");
+const Discord = require("discord.js");
+const offline = require("../../mpassword/offline");
+const util = require("minecraft-server-util");
 
-const guildId = '826787118104838184'
-const WOKCommands = require('wokcommands')
+const guildId = "826787118104838184";
+const WOKCommands = require("wokcommands");
 
-const songThing = require('../../handlers/songhandler')
+const songThing = require("../../handlers/songhandler");
 
-module.exports = async (Discord, client, message) =>{
-
-  
-    let channel = await client.channels.cache.find(channel => channel.id == "957553977896095764")
-    channel.send("BarniBot online!")
-    console.log(`BarniBot is online`)
-    client.user.setActivity(`-help | ${client.guilds.cache.size} servers`, {type:"LISTENING"})
-    offline(Discord, client)
-    songThing(Discord, client)
-    
-
-    
-    
-}
+module.exports = async (Discord, client, message) => {
+  let channel = await client.channels.cache.find(
+    (channel) => channel.id == "957553977896095764"
+  );
+  channel.send("BarniBot online!");
+  console.log(`BarniBot is online`);
+  client.user.setActivity(`-help | ${client.guilds.cache.size} servers`, {
+    type: "LISTENING",
+  });
+  offline(Discord, client);
+  //songThing(Discord, client);
+};
